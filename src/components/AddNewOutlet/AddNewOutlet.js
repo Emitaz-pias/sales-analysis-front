c34e60bf88bd48eaffef4f4314f4204146250493
emtiazpias <emtiazpias383@gmail.com>
2023-07-15 10:55:21 +0600

    add new outlet form added

--- src/components/AddNewOutlet/AddNewOutlet.js
+++ src/components/AddNewOutlet/AddNewOutlet.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
 
 const AddNewOutlet = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
     return (
         <div>
-            <h2>add new outlet</h2>      
+            <h2>add new outlet</h2>     
+            <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Company Name" {...register("Company Name", {required: true})} />
+      <br/>
+      <input type="text" placeholder="Outlet Name" {...register("Outlet Name", {required: true})} />
+            <br/>
+
+      <input type="number" placeholder="GP" {...register("GP", {required: true})} />
+
+      <input type="submit" />
+    </form> 
         </div>
     );
 };
