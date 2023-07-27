5099f48c9950dabee62b135d8594b516751d3002
Emtiaz-pias <emtiazpias383@gmail.com>
2023-07-27 23:47:41 +0600

    show expriy query form build done

--- src/components/AddNewExpiry/AddNewExpiry.js
+++ src/components/AddNewExpiry/AddNewExpiry.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Row, Form, Col, Table } from 'react-bootstrap';
+import { Row, Form, Col } from 'react-bootstrap';
 import ExpiryForm from './ExpiryForm';
 
 
