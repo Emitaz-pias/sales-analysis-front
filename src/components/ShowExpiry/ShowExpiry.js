e9523584369dc4cc83f92e33fce30ba2c127faa1
emtiazpias <emtiazpias383@gmail.com>
2023-08-01 19:52:14 +0600

    trying still get data

--- src/components/ShowExpiry/ShowExpiry.js
+++ src/components/ShowExpiry/ShowExpiry.js
@@ -8,6 +8,7 @@ const ShowExpiry = () => {
   const [allOutlets, setAllOutlets] = useState([])
   const [selectedOutlet, setSelectedOutlet] = useState('')
 
+
   
   useEffect(() => {
     axios
@@ -23,6 +24,127 @@ const ShowExpiry = () => {
         const handleOutletSelect = (event) => {
           setSelectedOutlet(event.target.value)
       }
+      // calculation part
+    
+// Function to calculate the percentage of expired products against delivered products
+// Function to calculate the percentage of expired products against delivered products
+// Function to calculate the percentage of expired products against delivered products
+// Function to calculate the percentage of expired products against delivered products
+// Function to calculate the percentage of expired products against delivered products
+// one of the rihgt fucntion//
+// function calculateExpiredPercentage(deliveryData, expiryData) {
+//   const result = [];
+
+//   deliveryData.forEach((deliveredProduct) => {
+//     const { deliveredProducts } = deliveredProduct;
+
+//     deliveredProducts.forEach((product) => {
+//       const { productCode, productName, deliveryQuantity } = product;
+
+//       const expiredProduct = expiryData.find((item) => {
+//         return (
+//           item.expiredProducts &&
+//           item.expiredProducts.some(
+//             (expiredItem) =>
+//               expiredItem.productCode === productCode &&
+//               expiredItem.productName === productName
+//           )
+//         );
+//       });
+
+//       const expiredCount = expiredProduct
+//         ? expiredProduct.expiredProducts.reduce(
+//             (acc, expiredItem) =>
+//               acc +
+//               (expiredItem.productCode === productCode &&
+//               expiredItem.productName === productName
+//                 ? expiredItem.expiredQuantity
+//                 : 0),
+//             0
+//           )
+//         : 0;
+
+//       const totalExpiredProducts = expiredCount;
+//       const totalProducts = deliveryQuantity + totalExpiredProducts;
+//       const expiredPercentage =
+//         totalProducts === 0 ? 0 : (100 * (totalExpiredProducts / totalProducts));
+
+//       const existingProductIndex = result.findIndex(
+//         (item) =>
+//           item.productCode === productCode && item.productName === productName
+//       );
+
+//       if (existingProductIndex !== -1) {
+//         result[existingProductIndex].expiredPercentage = expiredPercentage;
+//         result[existingProductIndex].totalExpiredProducts = totalExpiredProducts;
+//       } else {
+//         result.push({
+//           productCode,
+//           productName,
+//           deliveryQuantity,
+//           totalExpiredProducts,
+//           expiredPercentage,
+//         });
+//       }
+//     });
+//   });
+
+//   return result;
+// }
+function calculateExpiredPercentage(deliveryData, expiryData) {
+  const result = [];
+
+  deliveryData.forEach((deliveredProduct) => {
+    const { deliveredProducts } = deliveredProduct;
+
+    deliveredProducts.forEach((product) => {
+      const { productCode, productName, deliveredQuantity } = product;
+
+      const expiredProduct = expiryData.find((item) => {
+        return (
+          item.expiredProducts &&
+          item.expiredProducts.some(
+            (expiredItem) =>
+              expiredItem.productCode === productCode &&
+              expiredItem.productName === productName
+          )
+        );
+      });
+
+      const expiredCount = expiredProduct
+        ? expiredProduct.expiredProducts.reduce(
+            (acc, expiredItem) =>
+              acc +
+              (expiredItem.productCode === productCode &&
+              expiredItem.productName === productName
+                ? expiredItem.expiredQuantity
+                : 0),
+            0
+          )
+        : 0;
+
+      const totalProducts = deliveredQuantity + expiredCount;
+      const expiredPercentage =
+        totalProducts === 0 ? 0 : (100 * (expiredCount / totalProducts));
+
+      result.push({
+        productCode,
+        productName,
+        deliveredQuantity,
+        expiredQuantity: expiredCount,
+        expiredPercentage,
+      });
+    });
+  });
+
+  return result;
+}
+
+
+
+
+      
+      //
       const onSubmit =(data) => {
         const expriyQuery ={
           outlet:selectedOutlet,
@@ -34,11 +156,16 @@ const ShowExpiry = () => {
           params: expriyQuery
         })
         .then((response) => {
-          console.log(response.data,'is the data')})
+     console.log(  calculateExpiredPercentage(response.data.deliveryData,response.data.expiryData))
+})
+          
         .catch((error) => {
           console.error("Error getting data:", error);
         })
       };
+ 
+
+
   return (
     <section>
       <h2 className='text-danger text-center'>show expiry</h2>
